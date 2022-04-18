67cf2678bfb1d79336cf4a3c83bef6a3b23c3c40
Nurulaisyah1 <93265935+Nurulaisyah1@users.noreply.github.com>
2022-04-18 14:48:40 +0700

    Add files via upload

---
+++ facebook.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+import org.w3c.dom.Text;
+public class Facebook{
+    private static void printTitle(){
+        System.out.println("Facebook");
+    }
+public jumlahuser(int user){
+    this.user=us;
+    user=us.length();
+    System.out.println(user);
+}
+
+public  komentar (String komentar,int komentr) {
+    this.komentar=komen;
+    this.komentr=komentr;
+    Scanner inputuser = new Scanner(System.in);
+    String komen= inputuser.nextLine(); 
+    System.out.println("tulis komentar");
+    komentar= komen.length();
+    System.out.println(komentar);
+
+}
+    public static void main(String[] args) {
+        public void usr() {
+            Scanner user= new Scanner(System.in);
+            System.out.println((user));
+            
+        }{
+            System.out.println("jumlah like");
+        }
+    }
+}
